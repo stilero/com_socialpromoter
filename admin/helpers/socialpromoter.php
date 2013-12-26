8312bc91624858dc406595ae01d0ed5401ece4f5
Daniel Eliasson <daniel@stilero.com>
2013-12-26 12:28:03 +0100

    Added main Helper class

---
+++ admin/helpers/socialpromoter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Helper Class for Social Promoter
+ *
+ * @version  1.0
+ * @package Stilero
+ * @subpackage com_socialpromoter
+ * @author Daniel Eliasson <daniel at stilero.com>
+ * @copyright  (C) 2013-dec-26 Stilero Webdesign (http://www.stilero.com)
+ * @license	GNU General Public License version 2 or later.
+ * @link http://www.stilero.com
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access'); 
+
+class Socialpromoter{
+    
+}
