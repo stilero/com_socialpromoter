8e680a66bd685912feff34f9f9f779bf5d0d5cfb
Daniel Eliasson <daniel@stilero.com>
2014-01-08 13:03:42 +0100

    Comments displayed in accordion

--- site/views/gallery/tmpl/default.php
+++ site/views/gallery/tmpl/default.php
@@ -16,6 +16,7 @@ JHtml::_('bootstrap.framework');
 JHtml::_('jquery.ui');
 JHtml::script(JUri::root().'media/socialpromoter/assets/js/bootstrap-lightbox.min.js');
 JHTML::stylesheet(JUri::root().'media/socialpromoter/assets/css/bootstrap-lightbox.min.css');
+JHTML::stylesheet(JUri::root().'administrator/components/com_socialpromoter/assets/bootstrap/css/bootstrap.min.css');
 ?>
 <?php if(empty($this->items)): ?>
     <h1><?php echo JText::_('No Items found'); ?></h1>
@@ -23,20 +24,54 @@ JHTML::stylesheet(JUri::root().'media/socialpromoter/assets/css/bootstrap-lightb
     <div class="row">
         <?php $i=0; ?>
         <?php foreach ($this->items as $item) : ?>
-            <div class=""col-xs-6 col-md-3">
+            <div class="span6">
                  <div class="thumbnail">
-                 <a data-toggle="lightbox" href="#demoLightbox<?php echo $i; ?>">
+                 <a data-toggle="lightbox" href="#imgLightbox<?php echo $i; ?>">
                     <img src="<?php echo $item->url; ?>" class="img-responsive"/>
                 </a>
-                <?php echo $item->msg; ?>
+                     <p></p>
+                <p class="text-center">
+                    
+                        <i class="icon-thumbs-up"></i> 34  <i class="icon-comment"></i> 17  <a  class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $i; ?>" href="#socLightbox<?php echo $i; ?>"><i class="icon-resize-full"></i> Read Comments
+                    </a>
+                </p>
+                <div id="socLightbox<?php echo $i; ?>" class="accordion-body collapse">
+                    <table class="table table-condensed table-striped">
+                        <thead>
+                            <tr>
+                                <th><i class="icon-time"> </i></th>
+                                <th><i class="icon-comment"> </i></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>6d ago</td>
+                                <td>The Comment</td>
+                            </tr>
+                            <tr>
+                                <td>8d ago</td>
+                                <td>The Comment</td>
+                            </tr>
+                            <tr>
+                                <td>2w ago</td>
+                                <td>The Comment</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+                
             </div>
-            <div id="demoLightbox<?php echo $i++; ?>" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
+            </div>
+            <div id="imgLightbox<?php echo $i++; ?>" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
                 <div class='lightbox-content'>
+                    <div>
                         <img src="<?php echo $item->url; ?>">
-                        <div class="lightbox-caption"><p><span class="glyphicon glyphicon-thumbs-up"></span><span class="glyphicon glyphicon-comment"></span><?php echo $item->url; ?></p></div>
-                </div>
+                        <div class="lightbox-caption"><p>The caption</p></div>
+                    </div>
+                </div>  
             </div>
+            
+            
         <?php endforeach; ?>  
     </div>             
 <?php endif;?>
